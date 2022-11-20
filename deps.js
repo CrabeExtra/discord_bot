980d55f8f94d4b4d1aab07cc0291c92b1091eeec
Jude Bradbury <judebb4@gmail.com>
2022-11-20 11:10:49 +1000

    basic functionality

---
+++ deps.js
@@ -0,0 +1,7 @@
+export {
+    Client,
+    Message,
+    Intents
+} from 'https://deno.land/x/harmony/mod.ts';
+
+export { config } from "https://deno.land/x/dotenv/mod.ts";
---
+++ main.ts
@@ -0,0 +1,31 @@
+import {
+    Client,
+    Message,
+    Intents
+} from './deps.js';
+
+import { config } from "./deps.js"
+
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_TYPING"] });
+let {BOT_TOKEN, GUILD_ID_QG } = config();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+    const guildId = GUILD_ID_QG; // change per server
+
+    let commands;
+
+    commands = client.application?.commands;
+    
+    
+});
+
+client.on('messageCreate', (msg: any) => {
+    const content = msg.content;
+    //console.log(msg);
+    if(content.toLowerCase().includes("butler")) {
+        msg.reply('Gday cunt');
+    }
+});
+
+client.connect(BOT_TOKEN, Intents.None);
