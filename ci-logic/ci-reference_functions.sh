9031d7b09d954e3ba87693e19eec56ce351eb90b
Jude Bradbury <judebb4@gmail.com>
2024-07-27 23:23:11 +1000

    started separating out functionality for reusability and 'dryness'

---
+++ ci-logic/ci-reference_functions.sh
@@ -0,0 +1,30 @@
+concatenate_commands() {
+    local file=$1
+    local concatenated_commands=""
+
+    while IFS= read -r line; do
+        concatenated_commands+="$line"
+    done < "$file"
+
+    echo "$concatenated_commands wait"
+}
+
+run_commands() {
+    local ip=$1
+    local file=$2
+
+    local commands
+    commands=$(concatenate_commands "$file")
+
+    ssh -tt -o StrictHostKeyChecking=no "ubuntu@$ip" <<
+    EOF
+        $commands
+        exit 0
+    EOF
+}
+
+run_step() {
+    local ip=$1
+    local config_file=$2
+    run_remote_commands "$ip" "$config_file"
+}
