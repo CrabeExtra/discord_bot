5d10f374d8d398ee42d409fbf65f11e72ec3ea27
Jude Bradbury <judebb4@gmail.com>
2024-09-26 22:23:41 +1000

    test action working

--- slashCommands.js
+++ slashCommands.js
@@ -74,7 +74,7 @@ export const handleSlashCommands = async (interaction) => {
             let style = interaction.options.getBoolean("photo-realistic");
             let blackForestLabs = interaction.options.getBoolean("words-in-image");
             interaction.reply({
-                content: `I have begun crafting a piece of art depicting ${description}. I shall put it in the gallery when it is complete. photorealistic? ${style ? "yes" : "no"}. Will I account for words in the image? ${blackForestLabs ? "yes" : "no"}`,
+                content: `I have begun crafting a piece of fine art depicting ${description}. I shall put it in the gallery when it is complete. photorealistic? ${style ? "yes" : "no"}. Will I account for words in the image? ${blackForestLabs ? "yes" : "no"}`,
                 ephemeral: true
             });
             try {
