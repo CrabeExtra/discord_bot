77669f059a3f12a4f3e8347de630218c834e7da0
Jude Bradbury <judebb4@gmail.com>
2024-09-26 22:33:08 +1000

    logging the status

--- slashCommands.js
+++ slashCommands.js
@@ -113,6 +113,7 @@ export const handleSlashCommands = async (interaction) => {
                         log(taskId)
                         let jsonImgResponse = await response.json();
                         log(JSON.stringify(jsonImgResponse))
+                        log(JSON.stringify(response.status).replaceAll('"', ''));
                         if(JSON.stringify(response.status).replaceAll('"', '') === "Ready") {
                             log("Image ready, sending...")
                             imageUrl = JSON.stringify(jsonImgResponse.result.sample).replaceAll('"', '');
